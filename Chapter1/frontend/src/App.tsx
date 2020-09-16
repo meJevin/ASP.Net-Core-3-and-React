d7935f9f403bf1a610d15dcbed8d28f34f8cfc4d
Michael <mejevin@gmail.com>
2020-09-16 09:53:44 +0300

    add: home page

--- Chapter1/frontend/src/App.tsx
+++ Chapter1/frontend/src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
 import { Header } from './Header';
+import { HomePage } from './HomePage';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <Header/>
+      <HomePage/>
+      <h1>
+        Hi
+      </h1>
     </div>
   );
 }
