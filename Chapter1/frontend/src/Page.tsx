8f7e34eec6433a2c5394d77ab0a35386ec564089
Michael <mejevin@gmail.com>
2020-09-17 16:47:12 +0300

    Create Page.tsx

---
+++ Chapter1/frontend/src/Page.tsx
@@ -0,0 +1,27 @@
+/** @jsx jsx */
+import { css, jsx } from "@emotion/core";
+import React from "react";
+import { PageTitle } from "./PageTitle";
+import {QuestionList} from "./QuestionList";
+
+interface PageTitleProps {
+    title?: string;
+}
+
+export const Page: React.FC<PageTitleProps> = (props) => {
+    return (
+        <div
+        css={css`
+            margin: 50px auto 20px auto;
+            padding: 30px 20px;
+            max-width: 600px;
+        `}>
+            {props.title && <PageTitle>{props.title}</PageTitle>}
+            {props.children}
+        </div>
+    );
+};
+
+Page.defaultProps = {
+    title: "Enter a title", 
+}
