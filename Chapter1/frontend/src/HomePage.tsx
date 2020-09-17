68c0daac5735efe9fbf013e9b832d7d6965b8ebd
Michael <mejevin@gmail.com>
2020-09-17 16:47:10 +0300

    Update HomePage.tsx

--- Chapter1/frontend/src/HomePage.tsx
+++ Chapter1/frontend/src/HomePage.tsx
@@ -2,15 +2,14 @@ import React from "react";
 import { PrimaryButton } from "./Styles";
 /** @jsx jsx */
 import { css, jsx } from '@emotion/core';
+import { QuestionList } from "./QuestionList";
+import { getUnansweredQuestions, QuestionData } from "./QuestionData";
+import { Page } from "./Page";
+import { PageTitle } from "./PageTitle";
+import { Question } from "./Question";
 
 export const HomePage = () => (
-    <div 
-    css={css`
-    margin: 50px auto 20px auto;
-    padding: 30px 20px;
-    max-width: 600px;
-    `}
-    >
+    <Page title="niggers?">
         <div
             css={css`
             display: flex;
@@ -18,17 +17,26 @@ export const HomePage = () => (
             justify-content: space-between;
             `}
             >
-            <h2
-            css={css`
-            font-size: 15px;
-            font-weight: bold;
-            margin: 10px 0px 5px;
-            text-align: center;
-            text-transform: uppercase;
-            `}>
-                Unanswered questions
-            </h2>
+            <PageTitle>Unanswered questions</PageTitle>
             <PrimaryButton>Ask a question</PrimaryButton>
         </div>
-    </div>
+        
+        <QuestionList 
+            data={getUnansweredQuestions()}
+            renderQuestion={renderQuestionInList}/>
+    </Page>
 );
+
+function renderQuestionInList(qData: QuestionData): JSX.Element {
+    return (
+        <div>
+            <h5>
+                This is a custom question renderer!
+            </h5>
+            <span>
+                Question info: {qData.content}
+            </span>
+            <Question data={qData}/>
+        </div>
+    );
+}
