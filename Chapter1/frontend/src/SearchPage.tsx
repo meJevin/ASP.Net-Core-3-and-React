3b49b16df7530059eb7b31d2e160912dcbb146d4
Michael <mejevin@gmail.com>
2020-09-19 16:14:07 +0300

    add: search page

---
+++ Chapter1/frontend/src/SearchPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Page } from './Page'
+
+export const SearchPage = () => {
+    return (
+        <Page title="Search Page">
+            
+        </Page>
+    )
+}
