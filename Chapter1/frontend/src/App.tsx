3b717e7a8db3415a740dbb7e3593ee1ce9f3ea75
Michael <mejevin@gmail.com>
2020-09-19 16:19:36 +0300

    feat: basic path routing with react-router-dom

--- Chapter1/frontend/src/App.tsx
+++ Chapter1/frontend/src/App.tsx
@@ -4,17 +4,41 @@ import { HomePage } from './HomePage';
 /** @jsx jsx */
 import { css, jsx } from '@emotion/core';
 import { fontFamily, fontSize, gray2 } from './Styles';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import { SearchPage } from './SearchPage';
+import { AskPage } from './AskPage';
+import { SignInPage } from './SignInPage';
 
 const App: React.FC = () => {
   return (
-    <div className="App" css={css`
-      font-family: ${fontFamily};
-      font-size: ${fontSize};
-      color: ${gray2};
-    `}>
-      <Header/>
-      <HomePage/>
-    </div>
+    <BrowserRouter>
+      <div className="App" 
+      css={css`
+        font-family: ${fontFamily};
+        font-size: ${fontSize};
+        color: ${gray2};
+      `}>
+
+        <Header/>
+
+        <Route exact path="/">
+          <HomePage/>
+        </Route>
+
+        <Route path="/search">
+          <SearchPage/>
+        </Route>
+        
+        <Route path="/ask">
+          <AskPage/>
+        </Route>
+        
+        <Route path="/signin">
+          <SignInPage/>
+        </Route>
+      </div>
+    </BrowserRouter>
   );
 }
 
