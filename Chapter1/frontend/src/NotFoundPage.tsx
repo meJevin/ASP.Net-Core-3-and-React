3dc8c42bd853c74a928b6fdfa84def55b298c138
Michael <mejevin@gmail.com>
2020-09-19 22:20:08 +0300

    Create NotFoundPage.tsx

---
+++ Chapter1/frontend/src/NotFoundPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Page } from './Page'
+
+export const NotFoundPage: React.FC = () => {
+    return (
+        <Page title="Not Found!">
+            
+        </Page>
+    )
+}
