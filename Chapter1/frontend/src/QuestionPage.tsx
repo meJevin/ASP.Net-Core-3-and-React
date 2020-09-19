35ec4f5a62dc21ec276598cc10c98f9b1e17e10d
Michael <mejevin@gmail.com>
2020-09-19 23:17:42 +0300

    feat: route params for question page

--- Chapter1/frontend/src/QuestionPage.tsx
+++ Chapter1/frontend/src/QuestionPage.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { RouteComponentProps } from 'react-router-dom'
 import { Page } from './Page'
 
-export const QuestionPage: React.FC = () => {
+interface RouteParams {
+    questionId: string;
+}
+
+export const QuestionPage: React.FC<RouteComponentProps<RouteParams>> = (props) => {
     return (
         <Page title="Question Page">
-            
+            Question {props.match.params.questionId} 
         </Page>
     )
 }
