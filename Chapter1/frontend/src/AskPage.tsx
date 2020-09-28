164db0e651bac1e4995aefe331d32ccbc4e4d79d
Michael <mejevin@gmail.com>
2020-09-28 10:53:48 +0300

    feat: basic ask question form

--- Chapter1/frontend/src/AskPage.tsx
+++ Chapter1/frontend/src/AskPage.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import { Page } from './Page'
+import { Form } from './Form';
+import { Field } from './Field';
 
 export const AskPage: React.FC = () => {
     return (
         <Page title="Ask Page">
-
+            <Form submitCaption="Submit Your Question">
+                <Field name="title" label="Title" type="Text" />
+                <Field name="content" label="Content" type="TextArea" />
+            </Form>
         </Page>
     )
 }
