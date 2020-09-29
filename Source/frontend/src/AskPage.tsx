327eb31229c8fea41de655c18671bdeda9a76203
Michael <mejevin@gmail.com>
2020-09-29 23:23:24 +0300

    feat: utilize validation for ask page form

--- Source/frontend/src/AskPage.tsx
+++ Source/frontend/src/AskPage.tsx
@@ -1,12 +1,33 @@
 import React from 'react'
 import { Page } from './Page'
-import { Form } from './Form';
+import { Form, MinLength, Required } from './Form';
 import { Field } from './Field';
 
 export const AskPage: React.FC = () => {
     return (
         <Page title="Ask Page">
-            <Form submitCaption="Submit Your Question">
+            <Form submitCaption="Submit Your Question"
+                validationRules={{
+                    title: [
+                        {
+                            validator: Required
+                        },
+                        {
+                            validator: MinLength,
+                            arg: 10
+                        },
+                    ],
+                    content: [
+                        {
+                            validator: Required
+                        },
+                        {
+                            validator: MinLength,
+                            arg: 50
+                        },
+                    ],
+                }}
+                >
                 <Field name="title" label="Title" type="Text" />
                 <Field name="content" label="Content" type="TextArea" />
             </Form>
