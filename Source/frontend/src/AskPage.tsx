a55cd272e1f1bd3d3158780c53121727e43e9860
Michael <mejevin@gmail.com>
2020-09-29 23:52:32 +0300

    feat: implement form submission for AskPage component

--- Source/frontend/src/AskPage.tsx
+++ Source/frontend/src/AskPage.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import { Page } from './Page'
-import { Form, MinLength, Required } from './Form';
+import { Form, MinLength, Required, SubmitResult, Values } from './Form';
 import { Field } from './Field';
+import { postQuestion } from './QuestionData';
 
 export const AskPage: React.FC = () => {
+
+    const handleSubmit = async (values: Values): Promise<SubmitResult> => {
+        const question = await postQuestion({
+            title: values.title,
+            content: values.content,
+            userName: "Michael",
+            created: new Date(),
+        });
+
+        return { success: question ? true : false };
+    };
+
     return (
         <Page title="Ask Page">
             <Form submitCaption="Submit Your Question"
@@ -27,9 +40,12 @@ export const AskPage: React.FC = () => {
                         },
                     ],
                 }}
+                onSubmit={handleSubmit}
+                failureMessage="There was a problem with your question"
+                successMessage="Your question was successfully submitted"
                 >
-                <Field name="title" label="Title" type="Text" />
-                <Field name="content" label="Content" type="TextArea" />
+                    <Field name="title" label="Title" type="Text" />
+                    <Field name="content" label="Content" type="TextArea" />
             </Form>
         </Page>
     )
