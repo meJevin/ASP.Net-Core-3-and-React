342bc9bfd570655266253d1ad532b8d3f3163135
Michael <mejevin@gmail.com>
2020-10-15 23:39:17 +0300

    add: IDataRepository with basic Read operations

---
+++ Source/backend/Data/IDataRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.Data.Models;
+
+namespace WebAPI.Data
+{
+    public interface IDataRepository
+    {
+        IEnumerable<QuestionGetManyResponse> GetQuestions();
+        IEnumerable<QuestionGetManyResponse> GetQuestionsBySearch(string search);
+        IEnumerable<QuestionGetManyResponse> GetUnansweredQuestions();
+        QuestionGetSingleResponse GetQuestion(int questionId);
+        bool QuestionExists(int questionId);
+        AnswerGetResponse GetAnswer(int answerId);
+    }
+}
