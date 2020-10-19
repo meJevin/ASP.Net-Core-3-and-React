195a7cb8c20ce89448ff5961ef6382df6a2262a3
Michael <mejevin@gmail.com>
2020-10-19 20:50:14 +0300

    add: IDataRepository registered in DI

--- Source/backend/Startup.cs
+++ Source/backend/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using WebAPI.Data;
 
 namespace WebAPI
 {
@@ -43,6 +44,8 @@ namespace WebAPI
             }
 
             services.AddControllers();
+
+            services.AddScoped<IDataRepository, DataRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
