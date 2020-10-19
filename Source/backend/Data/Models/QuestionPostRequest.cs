5120504fcda768318339d2031c37dc078079764a
Michael <mejevin@gmail.com>
2020-10-19 21:17:04 +0300

    add: model validation for question post request

--- Source/backend/Data/Models/QuestionPostRequest.cs
+++ Source/backend/Data/Models/QuestionPostRequest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,11 @@ namespace WebAPI.Data.Models
 {
     public class QuestionPostRequest
     {
+        [Required]
+        [StringLength(100)]
         public string Title { get; set; }
+
+        [Required(ErrorMessage = "Please add content")]
         public string Content { get; set; }
         public string UserId { get; set; }
         public string UserName { get; set; }
