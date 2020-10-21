40c382f6dedbb4fe9bab504c9abba47145ca8139
Michael <mejevin@gmail.com>
2020-10-21 16:28:33 +0300

    feat: Questions with paging implemented

--- Source/backend/Data/DataRepository.cs
+++ Source/backend/Data/DataRepository.cs
@@ -193,5 +193,26 @@ namespace WebAPI.Data
                 new { QuestionId = questionId }
             );
         }
+
+        public IEnumerable<QuestionGetManyResponse> GetQuestionsBySearchWithPaging(string search,
+            int pageNumber, int pageSize)
+        {
+            using var connection = new SqlConnection(_connectionString);
+
+            connection.Open();
+            var parameters = new
+            {
+                Search = search,
+                PageNumber = pageNumber,
+                PageSize = pageSize
+            };
+
+            return connection.Query<QuestionGetManyResponse>(
+                @"EXEC dbo.Question_GetMany_BySearch_WithPaging
+                @Search = @Search,
+                @PageNumber = @PageNumber,
+                @PageSize = @PageSize", parameters
+            );
+        }
     }
 }
