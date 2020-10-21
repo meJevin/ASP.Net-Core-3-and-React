11a77dab05fa22e81c32f013112b6fd70ad407ec
Michael <mejevin@gmail.com>
2020-10-21 14:42:43 +0300

    change: GetQuestionsWithAnswers for IDataRepository

--- Source/backend/Data/IDataRepository.cs
+++ Source/backend/Data/IDataRepository.cs
@@ -9,12 +9,12 @@ namespace WebAPI.Data
     public interface IDataRepository
     {
         IEnumerable<QuestionGetManyResponse> GetQuestions();
+        IEnumerable<QuestionGetManyResponse> GetQuestionsWithAnswers();
         IEnumerable<QuestionGetManyResponse> GetQuestionsBySearch(string search);
         IEnumerable<QuestionGetManyResponse> GetUnansweredQuestions();
         QuestionGetSingleResponse GetQuestion(int questionId);
         bool QuestionExists(int questionId);
         AnswerGetResponse GetAnswer(int answerId);
-
         QuestionGetSingleResponse PostQuestion(QuestionPostFullRequest question);
         QuestionGetSingleResponse PutQuestion(int questionId, QuestionPutRequest question);
         void DeleteQuestion(int questionId);
