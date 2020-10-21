0bd5cbe850f35a4e598a4258ab0e13a8fdbc4f00
Michael <mejevin@gmail.com>
2020-10-21 14:43:25 +0300

    change: question get many responce model for newly implement feat

--- Source/backend/Data/Models/QuestionGetManyResponse.cs
+++ Source/backend/Data/Models/QuestionGetManyResponse.cs
@@ -12,5 +12,6 @@ namespace WebAPI.Data.Models
         public string Content { get; set; }
         public string UserName { get; set; }
         public DateTime Created { get; set; }
+        public List<AnswerGetResponse> Answers { get; set; }
     }
 }
