ee92a04dd80d42acfc0e2052fbd6f5777d5f176c
Michael <mejevin@gmail.com>
2020-11-03 00:58:22 +0300

    add: authorization policy name constants

---
+++ Source/backend/Authorization/Constants/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Authorization.Constants
+{
+    public static class PolicyNames
+    {
+        public  const string MustBeQuestionAuthor = "MustBeQuestionAuthor";
+    }
+}
