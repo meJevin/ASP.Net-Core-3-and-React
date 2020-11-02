3c50f03a23ce6338ec450ca21fd86a4d688bfb10
Michael <mejevin@gmail.com>
2020-11-02 11:41:49 +0300

    add: Question cache interface

---
+++ Source/backend/Data/IQuestionCache.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.Data.Models;
+
+namespace WebAPI.Data
+{
+    public interface IQuestionCache
+    {
+        QuestionGetSingleResponse Get(int questionId);
+        void Remove(int questionId);
+        void Set(QuestionGetSingleResponse question);
+    }
+}
