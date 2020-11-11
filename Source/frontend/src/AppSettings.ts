76f61f7f3f9bb36b1be447a4faf0bc82826af1f9
Michael <mejevin@gmail.com>
2020-11-11 22:26:24 +0300

    add: AppSettings

--- Source/frontend/src/AppSettings.ts
+++ Source/frontend/src/AppSettings.ts
@@ -1,1 +1,11 @@
+import { Auth0ClientOptions } from "@auth0/auth0-spa-js";
+
 export const webAPIUrl = "https://localhost:44316/api/";
+
+export const authSettings: Auth0ClientOptions = {
+    domain: 'dev-uhgue5d5.eu.auth0.com',
+    client_id: 'jtdoBdy36tW6w7uF5BYcpaLXdeRrGLdy',
+    redirect_uri: window.location.origin + '/signin-callback',
+    scope: 'openid profile QandAAPI email',
+    audience: 'https://qanda',
+};
