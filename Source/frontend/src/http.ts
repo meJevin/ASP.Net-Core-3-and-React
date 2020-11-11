e3b597c30c9306c958cef8e94b79a8fe83e324ee
Michael <mejevin@gmail.com>
2020-11-11 23:49:30 +0300

    change: extend generic http to support access token and post

--- Source/frontend/src/http.ts
+++ Source/frontend/src/http.ts
@@ -3,6 +3,9 @@ import { webAPIUrl } from './AppSettings';
 
 export interface HttpRequest<REQB> {
     path: string;
+    method?: string;
+    body?: REQB;
+    accessToken?: string;
 }
 
 export interface HttpResponse<RESB> extends Response {
@@ -13,13 +16,32 @@ export const http = <REQB, RESB> (
     config: HttpRequest<REQB>
 ): Promise<HttpResponse<RESB>> => {
     return new Promise((resolve, reject) => {
-        const request = new Request(`${webAPIUrl}${config.path}`);
+        const request = new Request(`${webAPIUrl}${config.path}`, {
+            method: config.method || 'get',
+            headers: {
+                'Content-Type': "application/json",
+            },
+            body: config.body ? JSON.stringify(config.body) : undefined,
+        });
+
+        if (config.accessToken) {
+            request.headers.set(
+                "authorization",
+                `bearer ${config.accessToken}`
+            );
+        }
+
         let response: HttpResponse<RESB>;
 
         fetch(request)
         .then(res => {
             response = res;
-            return res.json();
+
+            if (res.headers.get("Content-Type") || ''.indexOf('json') > 0) {
+                return res.json();
+            } else {
+                resolve(response);
+            }
         })
         .then(body => {
             if (response.ok) {
