c1ccfa4946a604e68e36a8731fb5558b4054395b
Michael <mejevin@gmail.com>
2020-11-16 22:09:10 +0300

    cypress config

--- Source/frontend/cypress/support/commands.js
+++ Source/frontend/cypress/support/commands.js
@@ -1,3 +1,5 @@
+import "@testing-library/cypress";
+
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
