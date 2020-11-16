49aee2a17f55c8b3282042e247edcf06a0ef42d9
Michael <mejevin@gmail.com>
2020-11-16 03:53:40 +0300

    add: basic test for Question.tsx with mocked question and router

---
+++ Source/frontend/src/Question.test.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { render, cleanup } from "@testing-library/react";
+import { Question } from './Question';
+import { QuestionData } from './QuestionData';
+import { BrowserRouter } from 'react-router-dom';
+
+test("When the Question component is rendered, it should contain the correct data", () => {
+    const question: QuestionData = {
+        questionId: 1,
+        title: 'Title test',
+        content: 'Content test',
+        userName: 'User1',
+        created: new Date(2019, 1, 1),
+        answers: [],
+    };
+
+    const renderResult = render(
+        <BrowserRouter>
+            <Question data={question}>
+            </Question>  
+        </BrowserRouter>
+    );
+
+    const titleText = renderResult.getByText('Title test');
+    expect(titleText).not.toBeNull();
+
+    const contentText = renderResult.getByText('Content test');
+    expect(contentText).not.toBeNull();
+
+    const userText = renderResult.getByText(/User1/);
+    expect(userText).not.toBeNull();
+    
+    const dateText = renderResult.getByText(/2019/);
+    expect(dateText).not.toBeNull();
+});
