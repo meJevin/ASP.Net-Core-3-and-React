9fa2ab321d2426678336240682fffef769f4540f
Michael <mejevin@gmail.com>
2020-11-16 03:47:00 +0300

    add: more page.tsx tests

--- Source/frontend/src/Page.test.tsx
+++ Source/frontend/src/Page.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, cleanup } from "@testing-library/react";
 import { Page } from './Page';
 
-test("When the Page component is rendered, it should contain the correct title in the H2 element", () => {
+test("When the Page component is rendered with a title, it should contain the correct title in the H2 element", () => {
     const title = "Test title";
 
     const renderResult = render(
@@ -15,4 +15,24 @@ test("When the Page component is rendered, it should contain the correct title i
     expect(pageTitleHTMLElement).not.toBeNull();
     expect(pageTitleHTMLElement.tagName).toBe("H2");
     expect(pageTitleHTMLElement.innerHTML).toBe(title);
+});
+
+test("When the Page component is rendered with content, it should contain the correct content", () => {
+    const contentElement = (
+        <button>
+            Click me            
+        </button>
+    );
+
+    const renderResult = render(
+        <Page>
+            {contentElement}
+        </Page>
+    )
+
+    const pageTitleHTMLElement = renderResult.getByText("Click me");
+
+    expect(pageTitleHTMLElement).not.toBeNull();
+    expect(pageTitleHTMLElement.tagName).toBe("BUTTON");
+    expect(pageTitleHTMLElement.innerHTML).toBe("Click me");
 });
