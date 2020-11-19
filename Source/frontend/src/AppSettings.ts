918a0f09dc151ce6d72a9273bee8364e180e226e
Michael <mejevin@gmail.com>
2020-11-20 01:20:48 +0300

    add: server for production and staging for react app

--- Source/frontend/src/AppSettings.ts
+++ Source/frontend/src/AppSettings.ts
@@ -1,6 +1,16 @@
 import { Auth0ClientOptions } from "@auth0/auth0-spa-js";
 
-export const webAPIUrl = "https://localhost:44316/api/";
+export const server = 
+process.env.REACT_APP_ENV === 'production'
+? 'https://qandaapi.azurewebsites.net'
+
+: process.env.REACT_APP_ENV === 'staging'
+? 'https://stagingqandaapi.azurewebsites.net'
+
+: 'https://localhost:44316';
+
+
+export const webAPIUrl = `${server}/api/`;
 
 export const authSettings: Auth0ClientOptions = {
     domain: 'dev-uhgue5d5.eu.auth0.com',
